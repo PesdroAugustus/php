e79f7bf20331241bc70d11361aedfe8152142157
PesdroAugustus <51246133+PesdroAugustus@users.noreply.github.com>
2019-06-03 17:09:13 -0300

    Resumi as parada mermão

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
-echo "Inferno o Palavra! - Ta erado puttth";
-echo "Profira sons organizados com seu orifício bucal projetando formar a palavra que se refere à qualquer animal "
-echo "Para os de pouca falácia o texto acima quer dizer: Fala bixxx!";
+echo "Hell Blade Senuas Sacrifice";
+echo "Fala garotada marota";
 
 ?>
